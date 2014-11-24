d4330b92bb8292f25aeae1d1d70e95c32b3716e9
Rodrigo Morais <morais.rm@gmail.com>
2014-11-23 23:44:22 -0200

    Add tests to application route.

--- services-wiki-ember/app/routes/application.js
+++ services-wiki-ember/app/routes/application.js
@@ -2,7 +2,6 @@
 
 var ApplicationRoute = Ember.Route.extend({
     model: function () {
-        console.log('ApplicationRoute');
         return Ember.RSVP.hash({
             menus: this.store.findAll('menu')
         });
---
+++ services-wiki-ember/tests/unit/routes/application-test.js
@@ -0,0 +1,47 @@
+﻿import { test, moduleFor } from 'ember-qunit';
+ 
+var route;
+
+moduleFor('route:application', "Unit - ApplicationRoute", {
+    setup: function () {
+        var store = {
+            findAll: function(type){
+                return new Em.RSVP.Promise(function(resolve){
+                    resolve(['Services', 'Hubs']);
+                });
+            }
+        };
+
+        this.subject().set('store', store);
+    
+    },
+    teardown: function () {}
+});
+
+test("application route exists", function(){
+    ok(this.subject());
+});
+
+test("return menu data", function(){
+    this.subject().model().then(function(result){
+        equal(Object.keys(result)[0], 'menus');
+    });
+});
+
+test("return two menus", function(){
+    this.subject().model().then(function(result){
+        equal(result['menus'].length, 2);
+    });
+});
+
+test("verify if first menu is Services", function(){
+    this.subject().model().then(function(result){
+        equal(result['menus'][0], 'Services');
+    });
+});
+
+test("verify if last menu is Hubs", function(){
+    this.subject().model().then(function(result){
+        equal(result['menus'][1], 'Hubs');
+    });
+});
