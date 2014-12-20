4ccc7a7488e82faa0961c42287174f5ade73460a
Rodrigo Morais <morais.rm@gmail.com>
2014-12-20 19:59:05 -0200

    Add unit tests to models from hubs.

--- services-wiki-ember/tests/unit/routes/hubs-models-test.js
+++ services-wiki-ember/tests/unit/routes/hubs-models-test.js
@@ -11,3 +11,33 @@ moduleFor('route:hubs-models', "Unit - HubsModelsRoute", {
 test("HubsModels route exists", function(){
     ok(this.subject());
 });
+
+test("return hubModel data", function(){
+    this.subject().model({'model_id': 'ChatMessage'}).then(function(result){
+        equal(Object.keys(result)[0], 'model');
+    });
+});
+
+test("verify hubModel name", function(){
+    this.subject().model({'model_id': 'ChatMessage'}).then(function(result){
+        equal(result['model'].name, 'ChatMessage');
+    });
+});
+
+test("verify hubModel type", function(){
+    this.subject().model({'model_id': 'ChatMessage'}).then(function(result){
+        equal(result['model'].type, 'hub');
+    });
+});
+
+test("verify if hubModel contains properties", function(){
+    this.subject().model({'model_id': 'ChatMessage'}).then(function(result){
+        ok(result['model'].properties.length > 0);
+    });
+});
+
+test("verify if exist two properties in hubModel", function(){
+    this.subject().model({'model_id': 'ChatMessage'}).then(function(result){
+        equal(result['model'].properties.length, 4);
+    });
+});
