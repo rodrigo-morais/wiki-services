051c3c9c6e044741037fde099147b765235dc04e
Rodrigo Morais <morais.rm@gmail.com>
2014-12-30 10:06:30 -0200

    Add unit tests to properties from component menu-item.

---
+++ services-wiki-ember/tests/unit/components/menu-item-test.js
@@ -0,0 +1,341 @@
+import Ember from "ember";
+import { test, moduleForComponent } from 'ember-qunit';
+
+moduleForComponent('menu-item');
+
+test('verify if tag name is LI', function(){
+    var component = this.subject();
+
+    Ember.run(function(){
+        component
+            .set(
+                'model',
+                {}
+            )
+            .set(
+                'type_parent',
+                'model'
+            );
+    });
+
+    equal(this.$().context.tagName, 'LI');
+});
+
+test('verify name of item', function(){
+    var component = this.subject();
+
+    Ember.run(function(){
+        component
+            .set(
+                'model',
+                {
+                    name: 'Item'
+                }
+            )
+            .set(
+                'type_parent',
+                'model'
+            );
+    });
+
+    equal(component.get('name'), 'Item');
+});
+
+test('verify type of item', function(){
+    var component = this.subject();
+
+    Ember.run(function(){
+        component
+            .set(
+                'model',
+                {
+                    name: 'Item',
+                    type: 'model'
+                }
+            )
+            .set(
+                'type_parent',
+                'model'
+            );
+    });
+
+    equal(component.get('type'), 'model');
+});
+
+test('verify if item contain submenus', function(){
+    var component = this.subject();
+
+    Ember.run(function(){
+        component
+            .set(
+                'model',
+                {
+                    name: 'Item',
+                    type: 'model',
+                    submenus: [
+                        {
+                            name: 'Sub 1'
+                        },
+                        {
+                            name: 'Sub 2'
+                        }
+                    ]
+                }
+            )
+            .set(
+                'type_parent',
+                'model'
+            );
+    });
+
+    equal(component.get('submenus').length, 2);
+});
+
+test('verify if name of first submenu of item', function(){
+    var component = this.subject();
+
+    Ember.run(function(){
+        component
+            .set(
+                'model',
+                {
+                    name: 'Item',
+                    type: 'model',
+                    submenus: [
+                        {
+                            name: 'Sub 1'
+                        },
+                        {
+                            name: 'Sub 2'
+                        }
+                    ]
+                }
+            )
+            .set(
+                'type_parent',
+                'model'
+            );
+    });
+
+    equal(component.get('submenus')[0].name, 'Sub 1');
+});
+
+test('verify if name of last submenu of item', function(){
+    var component = this.subject();
+
+    Ember.run(function(){
+        component
+            .set(
+                'model',
+                {
+                    name: 'Item',
+                    type: 'model',
+                    submenus: [
+                        {
+                            name: 'Sub 1'
+                        },
+                        {
+                            name: 'Sub 2'
+                        }
+                    ]
+                }
+            )
+            .set(
+                'type_parent',
+                'model'
+            );
+    });
+
+    equal(component.get('submenus')[1].name, 'Sub 2');
+});
+
+test('verify path of item', function(){
+    var component = this.subject();
+
+    Ember.run(function(){
+        component
+            .set(
+                'model',
+                {
+                    name: 'Item',
+                    type: 'model',
+                    submenus: [
+                        {
+                            name: 'Sub 1'
+                        },
+                        {
+                            name: 'Sub 2'
+                        }
+                    ]
+                }
+            )
+            .set(
+                'type_parent',
+                'model'
+            );
+    });
+
+    equal(component.get('path'), '/model/Item');
+});
+
+test('verify if hasTypeParent property return true whem menu item contain this property', function(){
+    var component = this.subject();
+
+    Ember.run(function(){
+        component
+            .set(
+                'model',
+                {
+                    name: 'Item',
+                    type: 'model',
+                    submenus: [
+                        {
+                            name: 'Sub 1'
+                        },
+                        {
+                            name: 'Sub 2'
+                        }
+                    ]
+                }
+            )
+            .set(
+                'type_parent',
+                'model'
+            );
+    });
+
+    ok(component.get('hasTypeParent'));
+});
+
+test('verify if hasTypeParent property return false whem menu item not contain this property', function(){
+    var component = this.subject();
+
+    Ember.run(function(){
+        component
+            .set(
+                'model',
+                {
+                    name: 'Item',
+                    type: 'model',
+                    submenus: [
+                        {
+                            name: 'Sub 1'
+                        },
+                        {
+                            name: 'Sub 2'
+                        }
+                    ]
+                }
+            );
+    });
+
+    ok(!component.get('hasTypeParent'));
+});
+
+test('verify uri property from menu item', function(){
+    var component = this.subject();
+
+    Ember.run(function(){
+        component
+            .set(
+                'model',
+                {
+                    name: 'Item',
+                    type: 'model',
+                    submenus: [
+                        {
+                            name: 'Sub 1'
+                        },
+                        {
+                            name: 'Sub 2'
+                        }
+                    ]
+                }
+            )
+            .set(
+                'type_parent',
+                'service'
+            );
+    });
+
+    equal(component.get('uri'), 'serviceModel');
+});
+
+test('verify target property from menu item', function(){
+    var component = this.subject();
+
+    Ember.run(function(){
+        component
+            .set(
+                'model',
+                {
+                    name: 'Item',
+                    type: 'model',
+                    submenus: [
+                        {
+                            name: 'Sub 1'
+                        },
+                        {
+                            name: 'Sub 2'
+                        }
+                    ]
+                }
+            )
+            .set(
+                'type_parent',
+                'service'
+            );
+    });
+
+    equal(component.get('target'), 'service-model');
+});
+
+test('verify if hasChilds property return true when menu item contains submenus', function(){
+    var component = this.subject();
+
+    Ember.run(function(){
+        component
+            .set(
+                'model',
+                {
+                    name: 'Item',
+                    type: 'model',
+                    submenus: [
+                        {
+                            name: 'Sub 1'
+                        },
+                        {
+                            name: 'Sub 2'
+                        }
+                    ]
+                }
+            )
+            .set(
+                'type_parent',
+                'service'
+            );
+    });
+
+    ok(component.get('hasChilds'));
+});
+
+test('verify if hasChilds property return false when menu item not contains submenus', function(){
+    var component = this.subject();
+
+    Ember.run(function(){
+        component
+            .set(
+                'model',
+                {
+                    name: 'Item',
+                    type: 'model'
+                }
+            )
+            .set(
+                'type_parent',
+                'service'
+            );
+    });
+
+    ok(!component.get('hasChilds'));
+});
