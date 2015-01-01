4779efd6881e293fe80a7cbdf014b32a2176208f
Rodrigo Morais <morais.rm@gmail.com>
2015-01-01 09:40:34 -0200

    Add unit tests to template from bread-crumb template.

--- services-wiki-ember/tests/unit/components/bread-crumb-test.js
+++ services-wiki-ember/tests/unit/components/bread-crumb-test.js
@@ -59,3 +59,66 @@ test('verify if second item of path property is "Conversation"', function(){
 
     equal(component.get('path')[1], 'Conversation');
 });
+
+test('verify if first item in bread crumb be "Services"', function(){
+    var component = this.subject(),
+        jsonObj,
+        firstElement;
+
+    Ember.run(function(){
+        component
+            .set(
+                'paths',
+                'Services,Conversation'
+            ).set(
+                'page',
+                'Get'
+            );
+    });
+
+    firstElement = Ember.$(this.$().find('ol').find('li')[0]);
+    
+    equal(firstElement.text(), 'Services');
+});
+
+test('verify if second item in bread crumb be "Conversation"', function(){
+    var component = this.subject(),
+        jsonObj,
+        secondElement;
+
+    Ember.run(function(){
+        component
+            .set(
+                'paths',
+                'Services,Conversation'
+            ).set(
+                'page',
+                'Get'
+            );
+    });
+
+    secondElement = Ember.$(this.$().find('ol').find('li')[1]);
+    
+    equal(secondElement.text(), 'Conversation');
+});
+
+test('verify if third item in bread crumb be "Conversation"', function(){
+    var component = this.subject(),
+        jsonObj,
+        thirdElement;
+
+    Ember.run(function(){
+        component
+            .set(
+                'paths',
+                'Services,Conversation'
+            ).set(
+                'page',
+                'Get'
+            );
+    });
+
+    thirdElement = Ember.$(this.$().find('ol').find('li')[2]);
+    
+    equal(thirdElement.text(), 'Get');
+});
